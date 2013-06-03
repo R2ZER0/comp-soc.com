532a84813b3bef5c1622bd135d3be1fe0ddfceb6
Rikki Guy <rikki.guy@gmail.com>
2013-06-04 00:25:06 +0100

    Resources, start of a design, no vibelog

--- source/app.d
+++ source/app.d
@@ -1,6 +1,25 @@
-import std.stdio;
+import vibe.d;
 
-void main()
-{ 
-	writeln("Edit source/app.d to start your project.");
+void index(HTTPServerRequest req, HTTPServerResponse res)
+{
+    res.redirect("/home");
+}
+
+void home(HTTPServerRequest req, HTTPServerResponse res)
+{
+    res.renderCompat!("index.dt", typeof(res), "res")(res);
+}
+
+shared static this()
+{
+    auto router = new URLRouter;
+    router.get("/", &index);
+    router.get("/home", &home);
+    
+    router.get("/public/*", serveStaticFiles("."));
+    
+    auto settings = new HTTPServerSettings;
+    settings.port = 8080;
+    
+    listenHTTP(settings, router);
 }
